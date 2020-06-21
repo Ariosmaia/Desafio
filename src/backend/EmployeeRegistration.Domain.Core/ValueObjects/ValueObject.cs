e8ec601fb04b445d0e8aaeaa1bf286bb1230ff80
Allan <ariosmaia@yahoo.com.br>
2020-06-21 15:29:30 -0300

    Finalizando o dominio

---
+++ src/backend/EmployeeRegistration.Domain.Core/ValueObjects/ValueObject.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace EmployeeRegistration.Domain.Core.ValueObjects
+{
+    public abstract class ValueObject<T> : AbstractValidator<T> where T : ValueObject<T>
+    {
+        public abstract bool IsValid();
+
+        public ValidationResult ValidationResult { get; protected set; }
+
+        public ValueObject()
+        {
+            ValidationResult = new ValidationResult();
+        }
+    }
+}
---
+++ src/backend/EmployeeRegistration.Domain/Employees/EmployeeSkill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeRegistration.Domain.Employees
+{
+    public class EmployeeSkill
+    {
+        public Guid EmployeeId { get; private set; }
+        public Employee Employee { get; private set; }
+        public virtual Guid SkillId { get; private set; }
+        public virtual Skill Skill { get; private set; }
+
+    }
+}
